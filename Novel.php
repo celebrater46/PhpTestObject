a4ca70000b72b6e6513da87912e208dccbb2ed17
celebrater <tkgm45nbchk@gmail.com>
2022-01-19 14:31:50 +0900

    Showing title, path and caption succeeded

--- Novel.php
+++ Novel.php
@@ -3,6 +3,13 @@
 class Novel
 {
     public $title;
+    public $path; // "sample/"
     public $caption;
     public $episodes = [];
+
+    function __construct($title, $folder_name){
+        $this->title = $title;
+        $this->path = $folder_name . "/";
+        $this->caption = file($this->path . "caption.txt");
+    }
 }
--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
 
+require_once "Novel.php";
+
+$SampleNovel = new Novel("サンプル小説", "sample");
+
+echo $SampleNovel->title . PHP_EOL;
+echo $SampleNovel->path . PHP_EOL;
+
+var_dump($SampleNovel->caption);
